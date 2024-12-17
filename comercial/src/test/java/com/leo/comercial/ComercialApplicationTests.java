b7c912e790cff1631dd746f69592afb6c13ac2a8
Leonardo Silva <levasi.leonardo55@gmail.com>
2024-12-16 22:41:08 -0300

    Project created

---
+++ comercial/src/test/java/com/leo/comercial/ComercialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leo.comercial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComercialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
