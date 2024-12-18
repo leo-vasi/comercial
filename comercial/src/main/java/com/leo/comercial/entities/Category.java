1cb5cfe608df77101cf80f37d393c4efd0ad0c26
Leonardo Silva <levasi.leonardo55@gmail.com>
2024-12-18 15:03:46 -0300

    Many-to-many association with JoinTable

--- comercial/src/main/java/com/leo/comercial/entities/Category.java
+++ comercial/src/main/java/com/leo/comercial/entities/Category.java
@@ -5,10 +5,13 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -22,6 +25,9 @@ public class Category implements Serializable{
 	private Long id;
 	private String name;
 	
+	
+	@JsonIgnore
+	@ManyToMany(mappedBy = "categories")
 	private Set<Product> products = new HashSet<>();
 	
 	public Category () {}
@@ -52,6 +58,7 @@ public class Category implements Serializable{
 	public void setName(String name) {
 		this.name = name;
 	}
+	
 	
 	public Set<Product> getProducts() {
 		return products;
