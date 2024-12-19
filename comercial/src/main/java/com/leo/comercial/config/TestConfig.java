6fd908f12035d01de77df5d8ecaaea6ec2c32c40
Leonardo Silva <levasi.leonardo55@gmail.com>
2024-12-18 22:53:17 -0300

    Payment, one-to-one association

--- comercial/src/main/java/com/leo/comercial/config/TestConfig.java
+++ comercial/src/main/java/com/leo/comercial/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.leo.comercial.entities.Category;
 import com.leo.comercial.entities.Order;
 import com.leo.comercial.entities.OrderItem;
+import com.leo.comercial.entities.Payment;
 import com.leo.comercial.entities.Product;
 import com.leo.comercial.entities.User;
 import com.leo.comercial.entities.enums.OrderStatus;
@@ -81,6 +82,11 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 
 	}
 }
